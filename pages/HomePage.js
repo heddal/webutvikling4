41c373b2a3097327396d9a4bbeff7104f3978b7a
Solveig <solvehl@stud.ntnu.no>
2019-11-05 11:32:26 +0100

    #10 Tab-bar ferdig

---
+++ pages/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class HomePage extends Component{
+    render(){
+        return(
+           <Text> HomePage </Text>
+        )
+    }
+}
+
+export default HomePage;

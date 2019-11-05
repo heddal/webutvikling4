9bc704e474744f198060147eb0b72ce82b400d39
Solveig <solvehl@stud.ntnu.no>
2019-11-05 12:04:43 +0100

    #12 Card outline

---
+++ components/Card.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Card extends Component {
+    
+    render(){
+        const styles = StyleSheet.create({
+            container: {
+                borderRadius: 7,
+                shadowOffset: {
+                    width: 0.5, height: 0.5
+                },
+                shadowColor: '#AAAAAA',
+                shadowOpacity: 1,
+                backgroundColor: 'white',
+                margin: 10,
+                padding: 10,
+                alignItems: "center"
+            }
+        })
+        return(
+            <View style = {styles.container}> 
+                <View><Text> Bildet her </Text></View>
+                <View><Text> Bynavn her </Text></View>
+                <View><Text> Knapp her </Text></View>
+            </View>
+        )
+    }
+}
+
+export default Card;
--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Card from '../components/Card'
 
 class HomePage extends Component{
     render(){
         return(
-           <Text> HomePage </Text>
+           <Card />
         )
     }
 }

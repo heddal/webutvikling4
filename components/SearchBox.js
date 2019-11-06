fc2ebb4d27dabd61beabf04951f9031d4cc4c744
Solveig <solvehl@stud.ntnu.no>
2019-11-06 11:20:11 +0100

    #14 inputtext works

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 class SearchBox extends Component {
 
-    render(){
-        const style = {
-            backgroundColor: '#EEEEEE',
-            margin: 20,
-            borderRadius: 10,
-            padding: 3
+    constructor(props){
+        super(props)
+        this.state = {
+            searchWord: ""
+        }
+    }
 
+    search  = ()  => {
+        word = this.state.searchWord
+        console.log("HEIDHK", word)
+    }
+
+    handleSearchWord = (e) => {
+        var word = this.state.searchWord
+        if (e.nativeEvent.key !== "Backspace"){
+            word += e.nativeEvent.key
+        } else if (this.state.searchWord.length !== 0){
+            word = this.state.searchWord.substr(0, this.state.searchWord.length -1)
+        } else {
+            console.log("Ikke no å slette")
+        }
+        this.setState({ searchWord: word})
+        console.log(word)
+    }
+
+    render(){
+        this.search = this.search.bind(this)
+        this.handleSearchWord = this.handleSearchWord.bind(this)
+        const inputfield = {
+            backgroundColor: '#EEEEEE',
+            borderRadius: 10,
+            padding: 3,
+            flex: 1
         }
 
         return(
-            <TextInput style = {style}> </TextInput>
+            <View style = {{flexDirection: "row", margin: 20}}>
+                <TextInput style = {inputfield} onKeyPress = {(e) => this.handleSearchWord(e)} onSubmitEditing = {() => this.search()} />
+                <Icon name = "search" onPress = {() => this.search()}/>
+            </View>
         );
     }
 

1b078dd223769ddf5627849e36bcdb1ea828373b
Solveig <solvehl@stud.ntnu.no>
2019-11-08 09:21:55 +0100

    #5 Dropdown menu outline

---
+++ frontend/components/ContinentDD.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Alert } from 'react-native';
+import { Dropdown } from 'react-native-material-dropdown';
+
+class ContinentDropDown extends Component{
+
+    render(){
+        return(
+            <Dropdown />
+        )}
+
+}
+
+export default ContinentDropDown;
--- frontend/pages/ExplorePage.js
+++ frontend/pages/ExplorePage.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SearchBox from '../components/SearchBox'
+import ContinentDropDown from '../components/ContinentDD'
 
 class ExplorePage extends Component {
     render(){
         return(
             <View>
                 <SearchBox />
+                <ContinentDropDown />
             </View>
         )
     }

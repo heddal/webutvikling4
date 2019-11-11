2c4fbcb3e49c99e58aa62ba959b44c59427250ca
Hedda <hedda.langvik@gmail.com>
2019-11-11 14:26:15 +0100

    #13 backend all good in the hood

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -60,6 +60,7 @@ class Card extends Component {
             }
         })
         const { data } = this.state
+        console.log(data)
         const dataCards = data.map(dat => {
             return (
                 <TouchableHighlight key={dat._id} underlayColor = 'white' onPress = {() => Alert.alert("HEI")}>
